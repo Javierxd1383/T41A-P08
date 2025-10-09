e1e94894668b4a2ea3567556454dc721c12da705
Javierxd1383 <javiercruzz1383@gmail.com>
2025-10-08 19:44:31 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,34 +1,26 @@
 CREATE TABLE customers (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(120) NOT NULL,
-    email VARCHAR(120) NOT NULL UNIQUE,
-    city VARCHAR(100) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    city VARCHAR(100),
     signup_date DATE NOT NULL
 );
-CREATE INDEX idx_customers_city ON customers(city);
-CREATE INDEX idx_customers_signup_date ON customers(signup_date);
-
-CREATE TABLE products (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name VARCHAR(150) NOT NULL,
-    price NUMERIC(10,2) NOT NULL
-);
-CREATE INDEX idx_products_name ON products(name);
-
+ 
 CREATE TABLE orders (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    customer_id BIGINT NOT NULL REFERENCES customers(id),
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES customers(id),
     order_date DATE NOT NULL
 );
-CREATE INDEX idx_orders_order_date ON orders(order_date);
-CREATE INDEX idx_orders_customer_id ON orders(customer_id);
-
+ 
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    price NUMERIC(10,2) NOT NULL
+);
+ 
 CREATE TABLE order_items (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    order_id BIGINT NOT NULL REFERENCES orders(id),
-    product_id BIGINT NOT NULL REFERENCES products(id),
+    id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL REFERENCES orders(id),
+    product_id INT NOT NULL REFERENCES products(id),
     quantity INT NOT NULL
 );
-CREATE INDEX idx_order_items_order_id ON order_items(order_id);
-CREATE INDEX idx_order_items_product_id ON order_items(product_id);
-
