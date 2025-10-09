ba325594142a7f514042153d28f7cf35d172d8cd
Javierxd1383 <javiercruzz1383@gmail.com>
2025-10-08 19:45:12 -0600

    Update 03_query_data.sql

--- 03_query_data.sql
+++ 03_query_data.sql
@@ -1,23 +1,9 @@
-SELECT c.id, c.name, o.id AS order_id, o.order_date
+SET enable_hashjoin = OFF;
+SET enable_mergejoin = OFF;
+ 
+EXPLAIN ANALYZE
+SELECT c.name, o.order_date, p.name AS product_name, oi.quantity
 FROM customers c
-LEFT JOIN orders o ON o.customer_id = c.id
-ORDER BY c.id, o.order_date;
-
-SELECT o.id AS order_id, o.order_date, c.name AS customer_name,
-       p.name AS product_name, oi.quantity, p.price,
-       (oi.quantity * p.price) AS line_total
-FROM orders o
-JOIN customers c ON c.id = o.customer_id
-JOIN order_items oi ON oi.order_id = o.id
-JOIN products p ON p.id = oi.product_id
-ORDER BY o.id, p.name;
-
-SELECT c.id, c.name,
-       COALESCE(SUM(oi.quantity * p.price), 0)::NUMERIC(12,2) AS total_spent
-FROM customers c
-LEFT JOIN orders o ON o.customer_id = c.id
-LEFT JOIN order_items oi ON oi.order_id = o.id
-LEFT JOIN products p ON p.id = oi.product_id
-GROUP BY c.id, c.name
-ORDER BY total_spent DESC, c.id;
-
+JOIN orders o ON c.id = o.customer_id
+JOIN order_items oi ON o.id = oi.order_id
+JOIN products p ON oi.product_id = p.id;
